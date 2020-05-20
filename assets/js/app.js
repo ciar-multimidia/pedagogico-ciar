50cb69a3fd0f1ee4bd7420a9ee36503577a3b236
Ana Flávia Cador <aflaviah15@gmail.com>
2020-05-20 11:22:01 -0300

    Início de estruturação de artigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,5 @@
 jQuery(document).ready(function($) {
 
-	var headerPortal = $('#header-portal'),
-		altura_headerPortal = headerPortal.outerHeight(),
-		listaVolumes = $('#lista-volumes'),
-		viewport = jQuery(window).width();
 
-	if (viewport > 680) {
-		listaVolumes.css('min-height', 'calc(100vh - ' + altura_headerPortal + 'px )');
-	}
 
 });
